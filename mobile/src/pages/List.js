526bd618e7ac849c8ba0a4eaf75839d9e6864219
João Victor <cardoso.jvo@gmail.com>
2020-02-05 11:13:36 -0300

    notifications

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, SafeAreaView, AsyncStorage, ScrollView, Image, StyleSheet, StatusBar } from 'react-native';
+
+import SpotList from '../components/SpotList';
+import logo from '../assets/logo.png';
+
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(tech => tech.trim(''));
+      setTechs(techsArray);
+      // console.log('TechsArray=', techsArray);
+    })
+  }, []);
+
+  return (
+    <SafeAreaView style={style.container}>
+      <Image source={logo} style={style.logo} />
+      <ScrollView>
+        {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+const style = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+})
